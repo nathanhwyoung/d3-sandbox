cdb30d2b11f289f1858c989a77541cae814571e6
Nathan <nhwilcox@gmail.com>
2016-03-24 16:57:06 -0700

    add features to line.js

--- line.js
+++ line.js
@@ -1,23 +1,31 @@
-var h = 100;
-var w = 200;
+var h = 350;
+var w = 400;
 
 monthlySales = [
-	{"month":10, "sales":20},
-	{"month":20, "sales":14},
-	{"month":30, "sales":20},
-	{"month":40, "sales":21},
-	{"month":50, "sales":15},
-	{"month":60, "sales":22},
-	{"month":70, "sales":9},
-	{"month":80, "sales":6},
-	{"month":90, "sales":23},
-	{"month":100, "sales":7},
+	{"month":10, "sales":100},
+	{"month":20, "sales":130},
+	{"month":30, "sales":250},
+	{"month":40, "sales":300},
+	{"month":50, "sales":265},
+	{"month":60, "sales":225},
+	{"month":70, "sales":180},
+	{"month":80, "sales":120},
+	{"month":90, "sales":145},
+	{"month":100, "sales":130},
 ];
 
+// line ends
+function lineEnds (d) {
+	return [
+		{"x":10,"y":20},
+		{"x":100, "y":7}
+	];
+}
+
 var lineFun = d3.svg.line()
-	.x(function(d) {return d.month*2;})
-	.y(function(d) {return d.sales*2;})
-	.interpolate("linear");
+	.x(function(d) {return d.month*3;})
+	.y(function(d) {return h-d.sales;})
+	.interpolate("basis");
 
 var svg = d3.select("body").append("svg").attr({
 	width:w, height:h});
@@ -37,6 +45,18 @@ var labels = svg.selectAll("text")
 	.append("text")
 	.text(function(d){ return d.sales; })
 	.attr({
-		x: function(d){ return d.month*3; },
+		x: function(d){ return d.month*3-25; },
 		y: function(d){ return h-d.sales; },
+		"font-size": "12px",
+		"font-family": "sans-serif",
+		"fill": "#666666",
+		"text-anchor": "start",
+		"dy": ".35em",
+		"font-weight": function(d,i) {
+			if (i===0 || i===(monthlySales.length-1)) {
+				return "bold";
+			} else {
+				return "normal";
+			}
+		}
 	});
